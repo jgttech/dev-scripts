dea20e6365ab7539b128c9723d8ab732a7cce05e
jgttech <jgt.tech@protonmail.com>
2023-10-09 22:01:09 -0500

    Add an alternative 'killport' command.

--- utils.sh
+++ utils.sh
@@ -2,3 +2,8 @@ function port-kill() {
   lsof -i tcp:$1 | awk 'NR!=1 {print $2}' | xargs kill;
   sudo kill $(sudo lsof -i:$1 -t) &> /dev/null;
 }
+
+function killport() {
+  sudo -S fuser -k ${1}/tcp
+  sudo -S fuser -k ${1}/udp
+}
