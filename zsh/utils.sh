c29ab9543c6282f4f852a03be4ec9a3b70079ce8
kronos <kronos@kronoss-MacBook-Pro.local>
2024-06-08 02:12:20 -0500

    2024-06-08 02:12:19 - UPDATES

--- zsh/utils.sh
+++ zsh/utils.sh
@@ -1,9 +1,13 @@
-function port-kill() {
+function port-kill {
   lsof -i tcp:$1 | awk 'NR!=1 {print $2}' | xargs kill;
   sudo kill $(sudo lsof -i:$1 -t) &> /dev/null;
 }
 
-function killport() {
+function killport {
   sudo -S fuser -k ${1}/tcp
   sudo -S fuser -k ${1}/udp
+}
+
+function pihole {
+  ssh arez@192.168.1.112
 }
